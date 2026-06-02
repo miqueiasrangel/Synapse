ef9d361a95aa9dd4cff49e13cd9266fe23f5cfab
Hendrius Dass <hendriusfelix.dev@gmail.com>
2026-06-02 11:06:15 -0300

    fix: criado arquivo tsconfig + configuracao de arquivo dew build

--- synapse-frontend/vite.config.js
+++ synapse-frontend/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === "production" ? "/synapse_ti/" : "/",
+    server: {
+      hmr: true
+    },
+    build: {
+      outDir: "synapse_ti",
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      }
+    }
+  }
 })
